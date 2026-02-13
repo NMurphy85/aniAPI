f6aee7df5c2d2a1c54dd2f7a4eb00e44dd9ab5b7
NMurphy85 <murphy.nathant@gmail.com>
2026-02-12 23:30:46 -0500

    added footer

--- index.js
+++ index.js
@@ -1,67 +1,58 @@
 // http://www.omdbapi.com/?apikey=1989ac72&s=super
-let movies
-const movieWrapper = document.querySelector('.movies')
+let movies;
+const movieWrapper = document.querySelector(".movies");
 async function movieAPI(filter) {
-    movieWrapper.classList = " movies__loading"
-    if (!movies){
-        const web = await fetch("http://www.omdbapi.com/?apikey=1989ac72&s=super")
-        movies = await web.json()
-    }
-    movieWrapper.classList.remove("movies__loading")
-    
-    const web = await fetch("http://www.omdbapi.com/?apikey=1989ac72&s=super")
-    const movie = (await web.json())
-    
-    
-    
-    
-    if (filter === "LOW_TO_HIGH") {
-        movie.Search.sort((a, b) => a.Title.localeCompare(b.Title))
-    }
-    if (filter === "HIGH_TO_LOW") {
-        movie.Search.sort((a, b) => b.Title.localeCompare(a.Title))
-    }
-    if (filter === "YEAR--LOW") {
-        movie.Search.sort((a, b) => a.Year.localeCompare(b.Year))
-    }
-    if (filter === "YEAR--HIGH") {
-        movie.Search.sort((a, b) => b.Year.localeCompare(a.Year))
-    }
-    const movieList = document.querySelector(".user-list")
-         const movieCard = await (movie.Search)
-     movieList.innerHTML = 
-     movieCard.map((movie) => {
-        return `<div class="user-card">
+  movieWrapper.classList = " movies__loading";
+  if (!movies) {
+    const web = await fetch("http://www.omdbapi.com/?apikey=1989ac72&s=super");
+    movies = await web.json();
+  }
+  movieWrapper.classList.remove("movies__loading");
+
+  const web = await fetch("http://www.omdbapi.com/?apikey=1989ac72&s=super");
+  const movie = await web.json();
+
+  if (filter === "LOW_TO_HIGH") {
+    movie.Search.sort((a, b) => a.Title.localeCompare(b.Title));
+  }
+  if (filter === "HIGH_TO_LOW") {
+    movie.Search.sort((a, b) => b.Title.localeCompare(a.Title));
+  }
+  if (filter === "YEAR--LOW") {
+    movie.Search.sort((a, b) => a.Year.localeCompare(b.Year));
+  }
+  if (filter === "YEAR--HIGH") {
+    movie.Search.sort((a, b) => b.Year.localeCompare(a.Year));
+  }
+  const movieList = document.querySelector(".user-list");
+  const movieCard = await movie.Search;
+  movieList.innerHTML = movieCard
+    .map((movie) => {
+      return `<div class="user-card">
             <div class="user-card__container">
                 <h3>${movie.Title}</h3>
                 <p><b>Year</b> ${movie.Year}</p>
                 <p><b>imdbID</b> ${movie.imdbID}</p>
                 <p><b>Poster</b> <a href="${movie.Poster}" target="_blank">Movie Poster</a></p>
             </div>
-            </div>`
-
-        }).join("")
-        
-    } 
-    
-    
-
+            </div>`;
+    })
+    .join("");
+}
 
 function movieFilter(event) {
-    movieAPI(event.target.value);
-
+  movieAPI(event.target.value);
 }
-
 
 setTimeout(() => {
-    movieAPI();
-})
+  movieAPI();
+});
 
-function getMovie(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(movieAPI())
-        }, 1000)
+function getMovie() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(movieAPI());
+    }, 1000);
+  });
 }
-)}
-getMovie()
+getMovie();
